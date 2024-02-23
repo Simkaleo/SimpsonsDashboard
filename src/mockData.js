edb7af976a1e166122bd46470c3fdd65eb791576
Simone LaPray <32243132+Simkaleo@users.noreply.github.com>
2024-02-22 21:15:18 -0500

    updating sidebar

--- src/mockData.js
+++ src/mockData.js
@@ -9,15 +9,6 @@ export const mockTransactions = [
       "https://i.discogs.com/J4bH_-A4UcQHFSUBDyyqXbTzr7XWM8S0NfNoYgwXAiI/rs:fit/g:sm/q:90/h:400/w:400/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTE0MDAz/MTctMTMzNTcxNzQ3/Ni5wbmc.jpeg",
   },
   {
-    txId: "0315dsaa",
-    user: "moe_szyslak",
-    date: "2022-04-01",
-    cost: "133.45",
-    drinkOrder: "Chardonnay",
-    image:
-      "https://i.discogs.com/J4bH_-A4UcQHFSUBDyyqXbTzr7XWM8S0NfNoYgwXAiI/rs:fit/g:sm/q:90/h:400/w:400/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTE0MDAz/MTctMTMzNTcxNzQ3/Ni5wbmc.jpeg",
-  },
-  {
     txId: "01e4dsa",
     user: "marge_simpson",
     date: "2021-09-01",
@@ -31,7 +22,9 @@ export const mockTransactions = [
     user: "ned_flanders",
     date: "2022-11-05",
     cost: "200.95",
-    drinkOrder: "Duff",
+    drinkOrder: "Holy Water",
+    image:
+      "https://www.gyfted.me/_next/image?url=%2Fimg%2Fcharacters%2Fned-flanders.png&w=640&q=75",
   },
   {
     txId: "0a123sb",
@@ -39,6 +32,8 @@ export const mockTransactions = [
     date: "2022-11-02",
     cost: "13.55",
     drinkOrder: "Chardonnay",
+    image:
+      "https://static.wikia.nocookie.net/p__/images/a/ae/Lenny_Leonard.png/revision/latest/thumbnail/width/360/height/360?cb=20120614215512&path-prefix=protagonist",
   },
   {
     txId: "01e4dsa",
@@ -46,20 +41,8 @@ export const mockTransactions = [
     date: "2021-09-01",
     cost: "43.95",
     drinkOrder: "White Claw",
-  },
-  {
-    txId: "120s51a",
-    user: "moe_szyslak",
-    date: "2019-04-15",
-    cost: "24.20",
-    drinkOrder: "Duff",
-  },
-  {
-    txId: "0315dsaa",
-    user: "moe_szyslak",
-    date: "2022-04-01",
-    cost: "133.45",
-    drinkOrder: "Chardonnay",
+    image:
+      "https://static.wikia.nocookie.net/characters/images/4/49/Mona_Simpson_.png/revision/latest?cb=20191206211349",
   },
   {
     txId: "8b72x1z",
@@ -67,6 +50,8 @@ export const mockTransactions = [
     date: "2024-02-15",
     cost: "56.80",
     drinkOrder: "White Claw",
+    image:
+      "https://static.wikia.nocookie.net/simpsons/images/7/7e/President_Lisa.png/revision/latest/scale-to-width/360?cb=20200115014238",
   },
   {
     txId: "4c91y2w",
@@ -74,6 +59,8 @@ export const mockTransactions = [
     date: "2024-01-25",
     cost: "102.15",
     drinkOrder: "Duff",
+    image:
+      "https://static.wikia.nocookie.net/simpsons/images/1/1f/Mooch_Bart.png/revision/latest?cb=20200115014414",
   },
   {
     txId: "2a38z9p",
@@ -81,6 +68,8 @@ export const mockTransactions = [
     date: "2024-02-03",
     cost: "75.40",
     drinkOrder: "Chardonnay",
+    image:
+      "https://i.pinimg.com/736x/97/aa/90/97aa902489ec29c94be6b7e1ba550744.jpg",
   },
   {
     txId: "6r42s7q",
@@ -88,6 +77,8 @@ export const mockTransactions = [
     date: "2024-01-12",
     cost: "30.25",
     drinkOrder: "White Claw",
+    image:
+      "https://static.wikia.nocookie.net/simpsonstappedout/images/f/f8/Krusty_Icon.png/revision/latest/thumbnail/width/360/height/360?cb=20211029205644",
   },
   {
     txId: "5y13x2v",
@@ -95,13 +86,17 @@ export const mockTransactions = [
     date: "2024-02-10",
     cost: "48.60",
     drinkOrder: "Sangria",
+    image:
+      "https://static.wikia.nocookie.net/simpsonsfanon/images/b/ba/Selma_Bouvier.png/revision/latest?cb=20230909040017",
   },
   {
     txId: "9z28b1a",
-    user: "ned_flanders",
+    user: "patty_bouvier",
     date: "2024-01-19",
     cost: "87.75",
     drinkOrder: "Chardonnay",
+    image:
+      "https://lezwatchtv.com/wp-content/uploads/2016/07/Patty_Bouvier.png",
   },
   {
     txId: "3w91a8z",
@@ -109,6 +104,7 @@ export const mockTransactions = [
     date: "2024-02-08",
     cost: "64.90",
     drinkOrder: "White Claw",
+    image: "",
   },
   {
     txId: "7q49r3y",
@@ -137,5 +133,14 @@ export const mockTransactions = [
     date: "2024-02-03",
     cost: "75.40",
     drinkOrder: "Sangria",
+  },
+  {
+    txId: "0315dsaa",
+    user: "moe_szyslak",
+    date: "2022-04-01",
+    cost: "133.45",
+    drinkOrder: "Chardonnay",
+    image:
+      "https://i.discogs.com/J4bH_-A4UcQHFSUBDyyqXbTzr7XWM8S0NfNoYgwXAiI/rs:fit/g:sm/q:90/h:400/w:400/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTE0MDAz/MTctMTMzNTcxNzQ3/Ni5wbmc.jpeg",
   },
 ];
